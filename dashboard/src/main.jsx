b33e6b434a0bb208d7cec92afa38367113a722bd
Ayenew Girmay <ayenewgirmay21@gmail.com>
2025-11-20 22:25:47 +0300

    Upload new mediserve full project code

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,20 +1,12 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 export const Context = createContext({ isAuthenticated: false });
 
 const AppWrapper = () => {
-  // Load saved authentication from localStorage
-  const savedAuth = localStorage.getItem("auth") === "true";
-
-  const [isAuthenticated, setIsAuthenticated] = useState(savedAuth);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [admin, setAdmin] = useState({});
-
-  // Save auth state when changed
-  useEffect(() => {
-    localStorage.setItem("auth", isAuthenticated);
-  }, [isAuthenticated]);
 
   return (
     <Context.Provider
